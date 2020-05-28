041e4d40c9660f3e9bd6409f27baa9a558b75c90
Amita Pawar <app78810@ucmo.edu>
2020-05-27 22:43:24 -0500

    Update ArrayPractice.java

--- src/com/ds/practice/arrays/ArrayPractice.java
+++ src/com/ds/practice/arrays/ArrayPractice.java
@@ -11,37 +11,63 @@ public class ArrayPractice {
 	handle index out of bound scenario
 	*/
 	public static void printElement(int[] arr, int index) {
+			
+			if(index<=arr.length) {
+			{
+				System.out.println(arr[index]);
+			}
+			}
+			else
+			{
+				throw new IndexOutOfBoundsException();
+			}
+		}
 		
-	}
-	
-	/*
-	 * search given key in array and print index of 
-	 * the key if key exist in the array else print -1
-	 */
-	public static void search(int[] arr, int key) {
+		/*
+		 * search given key in array and print index of 
+		 * the key if key exist in the array else print -1
+		 */
+		public static void search(int[] arr, int key) {
 		
-	}
-	
-	/**
-	 * write method which will create another array and copy elements 
-	 * from passed array to newly created array and return newly created array
-	 */
-	public static int[] clone(int[] arr) {
+			int indexval = 0;
+			for(int i=0; i<arr.length; i++) {
+				indexval = arr[i];
+				if(key==indexval) {
+				System.out.println(indexval);
+				}
+								
+			}
+		}
 		
+		/**
+		 * write method which will create another array and copy elements 
+		 * from passed array to newly created array and return newly created array
+		 */
+		public static int[] clone(int[] arr) {
+			
+			int[] arr2 = arr.clone();
+			
+			System.out.print("Cloned Array is as follow: {");
+			for(int i=0; i<arr2.length; i++) {
+				System.out.print(arr2[i]);
+				System.out.print(",");	
+			}
+			System.out.print("}");
+			return arr2;
+			
+			}
 		
-	}
-	
-	public static void main(String[] args) {
-		int[] arr = {0,5,4,6,3,9};
-		printElement(arr, 2);
-		printElement(arr, 8);
-		
-		search(arr, 5);
-		search(arr, 11);
-		
-		int[] clone = clone(arr);
-		
-		System.out.println(clone);
-	}
+		public static void main(String[] args) {
+			int[] arr = {0,5,4,6,3,9};
+			printElement(arr, 2);
+			printElement(arr, 4);
+			
+			search(arr, 5);
+			search(arr, 9);
+			
+			//int[] clone = clone(arr);
+			
+			clone(arr);
+		}
 
 }
